1db7ca79bce56a11068c49d9ca1368a94850421f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-03-04 13:24:12 +0000

    Update guildMemberUpdate.js

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -7,6 +7,7 @@ module.exports = (client, oldMember, newMember) => {
             "<a:florVermelho:1079413753386369114> Obrigado pelo boost, agora você virou um grande nakama do servidor",
           description: `<a:Borboleta:1079413751570255944> **Obrigado pelo impulso ${newMember}, agora você  obteve certas habilidades** assim como o demônio do sangue ${newMember} adquiriu certas habilidades e **agora é um <@&939901531254321263>, de uma lida nos** **[benefícios](https://discord.com/channels/936656115524042823/937009537175662652/1017106693982785547)** **para ficar por dentro de todas as suas habilidades** <a:firered:1079413754695004191>`,
           color: 9379615,
+          image: "https://media.discordapp.net/attachments/1078043994845614162/1079892779515248681/ae_003.png?width=1171&height=624",
           footer: {
             text: "©️ 2023 永遠 Anime's Eternity Todos os direitos reservados.",
           },
