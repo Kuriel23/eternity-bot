b17f09a87562763c2509ee285e5a189ff89cfd6f
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-04-14 19:42:31 +0100

    feat: upgrade Eternity IA for anti-pollution chat

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -25,26 +25,60 @@ module.exports = async (client, message) => {
     message.content.includes(`<@${client.user.id}>`) ||
     (replied !== null ? replied.author.id === client.user.id : 0)
   ) {
-    const youchatwrapper = require("@codernocook/youchatwrapper");
-    youchatwrapper.cloudflare_message_bypass = true;
-    youchatwrapper.cloudflare_retry_limit = 5;
-    youchatwrapper.apiKey = "FPWETB472RRB9L67U32US47RBW1D5Y0GH8M";
-    youchatwrapper.retry = true;
-    youchatwrapper.retry_limit = 3;
+    message
+      .reply({
+        content:
+          "<a:LNE_branco_kira:940057252629471295> Vendo o Lyke no vídeo fazer merda dentro de mim...",
+      })
+      .then((msg) => {
+        const youchatwrapper = require("@codernocook/youchatwrapper");
+        youchatwrapper.cloudflare_message_bypass = true;
+        youchatwrapper.cloudflare_retry_limit = 5;
+        youchatwrapper.apiKey = "FPWETB472RRB9L67U32US47RBW1D5Y0GH8M";
+        youchatwrapper.retry = true;
+        youchatwrapper.retry_limit = 3;
 
-    youchatwrapper.chat(
-      "responde-me em português! " +
-        message.content.replace(`<@${client.user.id}>`, ""),
-      function (callback) {
-        message.reply(
-          callback
-            .replace("@everyone", "everyone")
-            .replace("@here", "here")
-            .replace("you.com", "Animes Eternity")
-            .replace("You.com", "Animes Eternity")
+        youchatwrapper.chat(
+          `responde-me em português! ${message.content.replace(
+            `<@${client.user.id}>`,
+            ""
+          )}`,
+          function (callback) {
+            if (callback.length < 500 || message.channel.isThread())
+              msg.edit({
+                content: callback
+                  .replace("@everyone", "everyone")
+                  .replace("@here", "here")
+                  .replace("you.com", "Animes Eternity")
+                  .replace("You.com", "Animes Eternity"),
+              });
+            else {
+              const chars =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+              let randomString = "";
+              for (let i = 0; i < 4; i++) {
+                const randomIndex = Math.floor(Math.random() * chars.length);
+                randomString += chars[randomIndex];
+              }
+              msg
+                .startThread({
+                  name: "AI Eternity #" + randomString,
+                  autoArchiveDuration: 60,
+                  reason: "Ativado sistema para evitar poluição do chat.",
+                })
+                .then((thread) =>
+                  thread.send({
+                    content: callback
+                      .replace("@everyone", "everyone")
+                      .replace("@here", "here")
+                      .replace("you.com", "Animes Eternity")
+                      .replace("You.com", "Animes Eternity"),
+                  })
+                );
+            }
+          }
         );
-      }
-    );
+      });
   }
 
   if (message.guild.id === process.env.GUILD_ID) {
