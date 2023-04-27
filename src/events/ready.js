32d35f7568a1ec702061568845396c37e1b46b56
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-04-28 00:51:10 +0100

    feat: update routes types to v10

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { Routes } = require("discord-api-types/v10");
 const { readdirSync } = require("fs");
 require("dotenv").config();
 const { ChalkAdvanced } = require("chalk-advanced");
