996db46558eeaac1fa0f75d60abbe957b5886253
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-04-30 02:25:42 +0100

    feat: delete message after 5 seconds

--- src/buttons/rp.js
+++ src/buttons/rp.js
@@ -1,6 +1,8 @@
 module.exports = async (client, interaction) => {
-  interaction.reply({
-    content: `Esta parceria foi negada por <@${interaction.member.id}>!`,
-  });
+  interaction
+    .reply({
+      content: `Esta parceria foi negada por <@${interaction.member.id}>!`,
+    })
+    .then((msg) => setTimeout(() => msg.delete(), 5000));
   interaction.message.delete();
 };
