f3ff0f9b5c799f89a4b43277419f5e5ed0acb989
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-05-07 15:47:04 +0100

    feat: remove deprecated options on database

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { connect, Schema, model, set } = require("mongoose");
 const { ChalkAdvanced } = require("chalk-advanced");
 set("strictQuery", true);
-connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true })
+connect(process.env.db, {})
   .then(() =>
     console.log(
       `${ChalkAdvanced.gray(">")} ${ChalkAdvanced.green(
