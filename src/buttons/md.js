2220c410b123114f906d037e5b553eca4a79191e
! Lyke <78686667+Grandelukao@users.noreply.github.com>
2023-05-29 18:28:17 -0300

    Create md.js

---
+++ src/buttons/md.js
@@ -0,0 +1,76 @@
+const { PermissionFlagsBits } = require("discord.js");
+const discord = require("discord.js");
+
+module.exports = async (client, interaction) => {
+  const tagger = interaction.user.tag;
+  if (interaction.guild.channels.cache.find((c) => c.name === `${tagger}`)) {
+    const c = interaction.guild.channels.cache.find(
+      (c) => c.name === `${tagger}-clinica`
+    );
+    interaction.reply({
+      content: `Você já possui um ticket aberto em ${c}.`,
+      ephemeral: true,
+    });
+  } else {
+    interaction.guild.channels
+      .create({
+        name: `${tagger}-mudae`,
+        type: 0,
+        parent: "937014237082038333",
+        permissionOverwrites: [
+          {
+            id: interaction.guild.id,
+            deny: [PermissionFlagsBits.ViewChannel],
+          },
+          {
+            id: interaction.user.id,
+            allow: [
+              PermissionFlagsBits.ViewChannel,
+              PermissionFlagsBits.SendMessages,
+              PermissionFlagsBits.AttachFiles,
+              PermissionFlagsBits.AddReactions,
+            ],
+          },
+          {
+            id: "1020139519850336338",
+            allow: [
+              PermissionFlagsBits.ViewChannel,
+              PermissionFlagsBits.SendMessages,
+              PermissionFlagsBits.AttachFiles,
+              PermissionFlagsBits.AddReactions,
+            ],
+          },
+        ],
+      })
+      .then((c) => {
+        interaction.reply({
+          content: `Seu ticket foi aberto em ${c}.`,
+          ephemeral: true,
+        });
+
+        const embed = new discord.EmbedBuilder()
+          .setAuthor({
+            name: interaction.guild.name,
+            iconURL: interaction.guild.iconURL({ dynamic: true }),
+          })
+          .setColor(client.cor)
+          .setDescription(
+            `Olá, ${interaction.user.username}, boas vindas ao seu ticket!\nAguarde alguns instantes algum adm mudae irá aparecer para lhe ajudar`
+          );
+
+        const botao = new discord.ActionRowBuilder().addComponents(
+          new discord.ButtonBuilder()
+            .setCustomId("ft")
+            .setEmoji("🔒")
+            .setLabel("Fechar Ticket")
+            .setStyle(2)
+        );
+
+        c.send({
+          content: "<@&1000471228903067699>",
+          embeds: [embed],
+          components: [botao],
+        }).then((msg) => msg.pin());
+      });
+  }
+};
