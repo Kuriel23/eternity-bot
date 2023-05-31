60db6b7d4da909230078c98472f0560fb6870363
! Lyke <78686667+Grandelukao@users.noreply.github.com>
2023-05-31 07:07:46 -0300

    Update embedmudae.js

--- src/messages/embedmudae.js
+++ src/messages/embedmudae.js
@@ -15,9 +15,9 @@ module.exports = async (client, message) => {
   message.channel.send({
     embeds: [
       {
-        title: "Abra um ticket para atendimento!",
+        title: "**<:dec4:1015372877735923782>__Abra um ticket para atendimento!__**",
         description:
-          "<:mugiwarasetr:980567123183157268> `Abra tickets para receber apoio`\n\n<:mugiwarasetr:980567123183157268> Seja atendido para **suporte** ou **denúncia**, clicando no **menu** abaixo\n\n<:mugiwarasetr:980567123183157268> **Não use o nosso sistema para brincadeiras, dessa forma terão que ser tomadas providências necessárias.**",
+          " <:dec3:1015372950603567194>Abra tickets para receber apoio de um dos nossos mudae adm\n\n<:dec3:1015372950603567194> Seja atendido para **suporte** ou **denúncia**, clicando no **menu** abaixo\n\n<:dec3:1015372950603567194>  **Não use o nosso sistema para brincadeiras, dessa forma terão que ser tomadas providências necessárias.**",
         color: parseInt("#d65e9a".slice(1), 16),
         image: {
           url: "https://i.imgur.com/yE6LROD.jpg",
