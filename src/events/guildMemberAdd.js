65c96a668e679ba4bdb0af9fc3e72e8c5c262c9a
Lyke ᵉᵗʳ <78686667+Lyke-etr@users.noreply.github.com>
2023-06-21 12:32:53 -0300

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -6,7 +6,7 @@ module.exports = async (client, member) => {
 
 	if (member.guild.id === "936656115524042823") {
 		client.channels.cache.get("936678638324170762").send({
-			content: `<a:Welcome:946180052893171722> **Seja bem vindo(a) ao ${member.guild.name} ${member.user}, leia nossas <#944704305934524416> se apresente em <#1007356127983452300> e venha conversa conosco!!! <a:ola_etr:1013891617679220859> <@&967068395226947644>**`,
+			content: `<a:Welcome:946180052893171722> **Seja bem vindo(a) a ${member.guild.name} ${member.user}, leia nossas <#944704305934524416> se apresente em <#1007356127983452300> e venha conversa conosco!!! <a:ola_etr:1013891617679220859> <@&967068395226947644>**`,
 		});
 		const superagent = require("superagent");
 		await superagent
