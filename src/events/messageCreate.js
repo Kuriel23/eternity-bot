b9d20c50695d3b8ba5fa0b0a6e0f220260c5ce03
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-07-18 00:32:10 +0100

    fixes: revert change

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -4,8 +4,6 @@ module.exports = async (client, message) => {
   if (message.guild === null) return;
   if (message.author.bot) return 0;
 
-  if (message.author.id === "799673406014095440") message.delete();
-
   if (
     (message.content.startsWith("pt?") &&
       message.author.id === "354233941550694400") ||
